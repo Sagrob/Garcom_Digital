adc3ecb9144d86f384369f4483204ae2ad5894e8
Sagrob <bieldiniz27@gmail.com>
2025-02-07 13:24:12 -0300

    Sprint 1 - Parte 2

--- menu/admin.py
+++ menu/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Item, Category 
-
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price')
+from .models import Category, Item, StockMovement
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description')
+    list_display = ('name', 'description')
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock')
+
+@admin.register(StockMovement)
+class StockMovementAdmin(admin.ModelAdmin):
+    list_display = ('item', 'movement_type', 'quantity', 'date')
+    list_filter = ('movement_type', 'date')
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item, StockMovement
+from .forms import StockMovementForm
 
-# Create your views here.
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'inventory/item_list.html', {'items': items})
+
+def stock_movement(request):
+    if request.method == 'POST':
+        form = StockMovementForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('item_list')
+    else:
+        form = StockMovementForm()
+    return render(request, 'inventory/stock_movement.html', {'form': form})
